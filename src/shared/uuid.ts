09a4a2f916f1d97b04981dbf40d144430c983a05
mdwit0r <mdwit0r@gmail.com>
2023-09-03 00:27:34 +0200

    refactor(uuid.ts): rename UUIDVersions to UUIDVersion feat(uuid.ts): add support for namespace parameter in generateUuidByVersion function refactor(uuid.ts): rename uuidList to UUIDVersion refactor(uuid.ts): rename UUIDLinks to UUIDLink refactor(uuid.ts): rename validateVersion to validateUUIDVersion

--- src/shared/uuid.ts
+++ src/shared/uuid.ts
@@ -1,28 +1,24 @@
 import { NIL, v1, v3, v4, v5 } from 'uuid';
 
-export type UUIDVersions = 'v1' | 'v3' | 'v4' | 'v5' | 'empty';
+export type UUIDVersion = 'v1' | 'v3' | 'v4' | 'v5' | 'empty';
 
-export type UUIDLinks = { link: UUIDVersions; label: string }[];
-
-export const generateUuidByVersion = (version: UUIDVersions): string => {
+export const generateUuidByVersion = (version: UUIDVersion, namespace: string): string => {
 	switch (version) {
 		case 'v1':
 			return v1();
 		case 'v3':
-			return v3('uuid.tools', v3.DNS);
+			return v3(namespace, v3.DNS);
 		case 'v4':
 			return v4();
 		case 'v5':
-			return v5('uuid.tools', v5.DNS);
+			return v5(namespace, v5.DNS);
 		default:
 			return NIL;
 	}
 };
 
-const uuidList: UUIDVersions[] = ['v1', 'v3', 'v4', 'v5', 'empty'];
+export const uuidList: UUIDVersion[] = ['v1', 'v3', 'v4', 'v5', 'empty'];
 
-const uuidLinks: UUIDLinks = uuidList.map(link => ({ link, label: '/' + link }));
-
-export const validateVersion = (version: UUIDVersions): boolean => {
+export const validateVersion = (version: UUIDVersion): boolean => {
 	return uuidList.includes(version);
 };
