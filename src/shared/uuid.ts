b06795c8d2b873f22b8f572d9aa2339a556329d2
mdwit0r <mdwit0r@gmail.com>
2023-09-02 22:57:06 +0200

    feat(uuid.ts): add UUID generation and validation functions
    This commit adds a new file `uuid.ts` which contains functions for generating and validating UUIDs. The `generateUuidByVersion` function takes a version parameter and returns a UUID based on that version. The supported versions are 'v1', 'v3', 'v4', 'v5', and 'empty'. The `validateVersion` function checks if a given version is valid.
    
    Additionally, the file exports a `UUIDLinks` type which represents a list of UUID versions with their corresponding labels.

--- src/shared/uuid.ts
+++ src/shared/uuid.ts
@@ -1,10 +1,12 @@
 import { NIL, v1, v3, v4, v5 } from 'uuid';
 
 export type UUIDVersions = 'v1' | 'v3' | 'v4' | 'v5' | 'empty';
+
+export type UUIDLinks = { link: UUIDVersions; label: string }[];
+
 export const generateUuidByVersion = (version: UUIDVersions): string => {
 	switch (version) {
 		case 'v1':
-			console.log(v1());
 			return v1();
 		case 'v3':
 			return v3('uuid.tools', v3.DNS);
@@ -15,4 +17,12 @@ export const generateUuidByVersion = (version: UUIDVersions): string => {
 		default:
 			return NIL;
 	}
+};
+
+const uuidList: UUIDVersions[] = ['v1', 'v3', 'v4', 'v5', 'empty'];
+
+const uuidLinks: UUIDLinks = uuidList.map(link => ({ link, label: '/' + link }));
+
+export const validateVersion = (version: UUIDVersions): boolean => {
+	return uuidList.includes(version);
 };
