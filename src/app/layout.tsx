df72c4491fd94dad22286152164751d7096280d5
mdwit0r <mdwit0r@gmail.com>
2023-09-03 06:00:48 +0200

    chore(package.json): remove unused dependencies refactor(layout.tsx): remove Yandex Metrika tracking code and noscript tag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,6 @@
 import { AppProvider } from '@/core/provider';
 import { DOMAIN } from '@/core/configs';
 import { Metadata } from 'next';
-import Head from 'next/head';
-import Script from 'next/script';
 
 export const metadata: Metadata = {
 	metadataBase: new URL(DOMAIN),
@@ -42,33 +40,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
 		<html lang="en-US">
-			<Head>
-				<Script
-					dangerouslySetInnerHTML={{
-						__html: `
-            (function(m,e,t,r,i,k,a){m[i]=m[i]||function(){(m[i].a=m[i].a||[]).push(arguments)};
-            m[i].l=1*new Date();
-            for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
-            k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
-            (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
-
-            ym(94820522, "init", {
-                clickmap:true,
-                trackLinks:true,
-                accurateTrackBounce:true,
-                webvisor:true
-            });
-            `,
-					}}
-				/>
-			</Head>
 			<body>
 				<AppProvider>{children}</AppProvider>
-				<noscript>
-					<div>
-						<img src="https://mc.yandex.ru/watch/94820522" />
-					</div>
-				</noscript>
 			</body>
 		</html>
 	);
