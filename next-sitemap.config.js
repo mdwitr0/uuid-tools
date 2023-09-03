ce73b37577d7863c74cf85d118a3de9d974a2504
mdwit0r <mdwit0r@gmail.com>
2023-09-03 02:27:25 +0200

    feat(jest.config.js): add Jest configuration file feat(jest.setup.js): add Jest setup file feat(next-sitemap.config.js): add additional paths to NextSitemapConfig

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -2,6 +2,17 @@
 const NextSitemapConfig = {
 	siteUrl: 'https://uuid.tools/',
 	generateRobotsTxt: true,
+	additionalPaths: async config => {
+		const result = [];
+
+		result.push({ loc: '/v1' });
+		result.push({ loc: '/v3' });
+		result.push({ loc: '/v4' });
+		result.push({ loc: '/v5' });
+		result.push({ loc: '/empty' });
+
+		return result;
+	},
 };
 
 module.exports = NextSitemapConfig;
