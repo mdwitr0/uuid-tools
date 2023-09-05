a293e3fe9e30bda550ed981af8eae4f8a05457c0
mdwit0r <mdwit0r@gmail.com>
2023-09-05 17:27:49 +0200

    chore(next-sitemap.config.js): update siteUrl to 'https://www.uuid.tools'

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,6 +1,6 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-	siteUrl: 'https://uuid.tools',
+	siteUrl: 'https://www.uuid.tools',
 	generateRobotsTxt: true,
 	additionalPaths: async config => {
 		const result = [];
