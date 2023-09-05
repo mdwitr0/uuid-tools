ea86e978f8dcafbfdd59e394b10ab855416bbf6e
mdwit0r <mdwit0r@gmail.com>
2023-09-05 15:37:17 +0200

    chore(next-sitemap.config.js): update sitemap configuration
    - Change siteUrl to 'https://uuid.tools' (remove trailing slash)
    - Add additional paths to the sitemap with their respective priorities and alternate references for different languages:
      - '/'
        - Priority: 1
        - Alternate reference: '/ru' with priority 1 and hreflang 'ru'
      - '/version/v1'
        - Priority: 0.8
        - Alternate reference: '/ru' with priority 0.8 and hreflang 'ru'
      - '/version/v3'
        - Priority: 0.8
        - Alternate reference: '/ru' with priority 0.8 and hreflang 'ru'
      - '/version/v4'
        - Priority: 0.8
        - Alternate reference: '/ru' with priority 0.8 and hreflang 'ru'
      - '/version/v5'
        - Priority:

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,15 +1,76 @@
 /** @type {import('next-sitemap').IConfig} */
 const NextSitemapConfig = {
-	siteUrl: 'https://uuid.tools/',
+	siteUrl: 'https://uuid.tools',
 	generateRobotsTxt: true,
 	additionalPaths: async config => {
 		const result = [];
 
-		result.push({ loc: '/v1' });
-		result.push({ loc: '/v3' });
-		result.push({ loc: '/v4' });
-		result.push({ loc: '/v5' });
-		result.push({ loc: '/empty' });
+		result.push({
+			loc: '/',
+			priority: 1,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 1,
+					hreflang: 'ru',
+				},
+			],
+		});
+		result.push({
+			loc: '/version/v1',
+			priority: 0.8,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 0.8,
+					hreflang: 'ru',
+				},
+			],
+		});
+		result.push({
+			loc: '/version/v3',
+			priority: 0.8,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 0.8,
+					hreflang: 'ru',
+				},
+			],
+		});
+		result.push({
+			loc: '/version/v4',
+			priority: 0.8,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 0.8,
+					hreflang: 'ru',
+				},
+			],
+		});
+		result.push({
+			loc: '/version/v5',
+			priority: 0.8,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 0.8,
+					hreflang: 'ru',
+				},
+			],
+		});
+		result.push({
+			loc: '/version/empty',
+			priority: 0.8,
+			alternateRefs: [
+				{
+					href: '/ru',
+					priority: 0.8,
+					hreflang: 'ru',
+				},
+			],
+		});
 
 		return result;
 	},
