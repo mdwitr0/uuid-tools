02f86439da3eac2e0844e4ff19fba98b1b338c07
mdwit0r <mdwit0r@gmail.com>
2023-09-10 11:34:52 +0200

    fix(provider.tsx): remove unused imports and variables feat(provider.tsx): remove support for RTL direction

--- src/core/provider.tsx
+++ src/core/provider.tsx
@@ -1,26 +1,16 @@
 'use client';
 
-import { ColorScheme, ColorSchemeProvider, createEmotionCache, MantineProvider } from '@mantine/core';
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core';
 import { ModalsProvider } from '@mantine/modals';
 import { Notifications } from '@mantine/notifications';
 import { useMediaQuery } from '@mantine/hooks';
 import { useEffect, useState } from 'react';
-import rtlPlugin from 'stylis-plugin-rtl';
 import RootStyleRegistry from './emotion';
 import { theme } from '@/core/theme';
 import WithYandexMetrika from '@/core/providers/with-yandex-metrika';
 import { Analytics } from '@vercel/analytics/react';
 
-const THEME_KEY = 'uuidtools-theme';
-const rtlCache = createEmotionCache({
-	key: 'mantine-rtl',
-	prepend: true,
-	stylisPlugins: [rtlPlugin],
-});
-
 export function AppProvider({ children }: { children: React.ReactNode }) {
-	const [dir, setDir] = useState<'rtl' | 'ltr'>('ltr');
-
 	const preferredColorScheme = useMediaQuery('(prefers-color-scheme: dark)');
 	const [colorScheme, setColorScheme] = useState<ColorScheme>(preferredColorScheme ? 'dark' : 'light');
 	useEffect(() => {
@@ -33,17 +23,11 @@ export function AppProvider({ children }: { children: React.ReactNode }) {
 
 	const toggleColorScheme = (value?: ColorScheme) => setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-	const toggleDir = () => {
-		const nextDir = dir === 'ltr' ? 'rtl' : 'ltr';
-		setDir(nextDir);
-		document.querySelector('html')!.setAttribute('dir', nextDir);
-	};
-
 	return (
 		<WithYandexMetrika>
 			<RootStyleRegistry>
 				<ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
-					<MantineProvider withGlobalStyles withNormalizeCSS theme={{ ...theme, colorScheme, dir }}>
+					<MantineProvider withGlobalStyles withNormalizeCSS theme={{ ...theme, colorScheme }}>
 						<ModalsProvider>{children}</ModalsProvider>
 						<Notifications />
 					</MantineProvider>
